c90615d89af56e3d791c0e8c598ca957d73e1dbb
Jacob White <jjw37@njit.edu>
2021-04-19 02:49:16 +0000

    added in js tests for UI, first test is good, second test fails

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render,fireEvent, waitFor, screen } from '@testing-library/react';
+import App from './App.js';
+import Signup from './Signup.js'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('changes to sign in page after clicking sign in button', () => {
+  const resultRender = render(<App />);
+  
+  const signInButton= screen.getByText('Sign in');
+  expect(signInButton).toBeInTheDocument();
+  
+  fireEvent.click(signInButton)
+  
+  expect(signInButton).not.toBeInTheDocument();
+  
+  const signInStatus= screen.getByText('Sign in with google');
+  expect(signInStatus).toBeInTheDocument();
+});
+
+test('Brings user to Dashboard after signing in', () => {
+  const resultRender = render(<Signup />);
+  
+  const LoginButton= screen.getByText('Sign in with Google');
+  expect(LoginButton).toBeInTheDocument();
+  const signupText = screen.getByText('Type in your miner username');
+  expect(signupText).toBeInTheDocument();
+  
+  fireEvent.click(LoginButton)
+  
+ // expect(LoginButton).not.toBeInTheDocument();
+  //expect(signupText).not.toBeInTheDocument();
+  
+  const DashboardText= screen.getByText('Current Miners');
+  expect(DashboardText).toBeInTheDocument();
 });
