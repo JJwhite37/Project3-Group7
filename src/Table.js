1689803a43035da738dec5c24cb1aa90b88feb6b
NickAbadio <na354@njit.edu>
2021-04-27 21:27:40 +0000

    Set up basics of hover for user info

--- src/Table.js
+++ src/Table.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Table.css';
+import { useState, useEffect } from 'react';
 
 function Table(props) {
   const { currentMiners } = props;
+  const [isShown, setIsShown] = useState(false);
 
   const result = [];
   let i;
   for (i = 0; i < currentMiners.length; i += 1) {
     result.push(
-      <tr>
+      <tr 
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}>
         <td>{currentMiners[i][0]}</td>
         <td>{currentMiners[i][1]}</td>
+        {isShown && (
+        <div>
+          testing
+        </div>
+        )}
         <br />
       </tr>,
       <br />,
