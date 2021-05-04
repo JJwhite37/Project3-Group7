28bb16f5b1d1ba65b04473dcdbbb6e73f9525585
Joseph Stapleton <ec2-user@ip-172-31-40-74.ec2.internal>
2021-05-04 18:34:51 +0000

    Created future section for landing Page.  Lasha provided content

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,14 +3,14 @@ import AboutEcommMiner from './AboutEcommMiner';
 function LandingPage() {
     return (
       <div>
-        <h1>Hello this is EcommMiner</h1>
+        <h1>Hello this is EcomMiner</h1>
         <h2>  Functionality: </h2>
         <p>
           This application was created as a MVP for tracking shares.  
           First, a person must login with their email and provide their worker_name.  New users will be able to log in but show 0 shares. 
         </p> 
         <p>
-          Once logged in, We display all the information from a flexpool api in an easy to understand Daahboard.  
+          Once logged in, we display all the information from a flexpool api in an easy to understand Dashboard.  
         </p>
         <p>
           All logged in miners will be able to see their own and the wallets statistics. 
@@ -25,13 +25,19 @@ function LandingPage() {
         
         <h2>  Why we matter: </h2>
         <p>
-          EcommMiner can be the future of selling computation. Using a dashboard like ours, other businesses will be able to keep track of all the people mining for them. This allows the customers to visually see how much they mine and an easy way to see who is actually a committed miner for that business.Abadio
+          EcomMiner can be the future of selling computation. Using a dashboard like ours, other businesses will be able to keep track of all the people mining for them. This allows the customers to visually see how much they mine and an easy way to see who is actually a committed miner for that business.Abadio
           
+        </p>
+        <h3>  How we could be used in the future: </h3>
+        <p>
+        Since, EcomMiner is a open source platform that makes it easy for communities or creators to get paid. 
+        Ecom miner is a great way to internationally source computation(mining Ethereum) for services from creators. 
+        Instead of literally throwing money at your screen (trust us, that doesn’t work), you can run you hardware(GPU) per month to pay for individual service's from simple company's raffle's, or even greater privileges like subscriptions.
         </p>
         
         <h2>  Who is it made by: </h2>
         <p>
-          EcommMiner was created by a collaboration between 4 senior CS students, Joseph Stapleton, Jacob White, Nick Abadio, Lasha Tavberidze.
+          EcomMiner was created by a collaboration between 4 senior CS students, Joseph Stapleton, Jacob White, Nick Abadio, Lasha Tavberidze.
         </p>
         <p> 
           We are very exited to showcase our knowledge and provide an actual working application that could be the future of commercial computation.
