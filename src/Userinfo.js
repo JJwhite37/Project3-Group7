0bb5a2aa2115321fa043c28684da0c2e38e82bdb
NickAbadio <na354@njit.edu>
2021-05-04 18:00:41 +0000

    finshed User info component ready to push to main

---
+++ src/Userinfo.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import { socket } from './App';
+
+export function UserInfo(props) {
+  const [myList, changeList] = useState([]);
+  const [isShown, setIsShown] = useState(false);
+  socket.emit('UserInfo', {name: props.name})
+  useEffect(() => {
+        socket.on('UserInfo', (data) => {
+            console.log('UserInfo returned ');
+            console.log(data);
+            changeList(data);
+            
+    });
+  });
+  
+  
+  return (
+    <div class="info">
+      <div>Current Effective Hashrate: {myList[0]}</div>
+      <div>Average Effective Hashrate: {myList[1]}</div>
+      <div>Current Reported Hashrate: {myList[2]}</div>
+      <div>Valid Shares: {myList[3]}</div>
+      <div>Stale Shares: {myList[4]}</div>
+      <div>Invalid Shares: {myList[5]}</div>
+    </div>
+  );
+}
+
+export default UserInfo;
